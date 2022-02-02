caa62a879f2fee04bdd5c518818ddfe4f812d93a
Sebastian <sebastianrubioquiroz@gmail.com>
2022-02-01 18:40:16 -0600

    Add Icons

--- BMI Calculator WatchKit Extension/ContentView.swift
+++ BMI Calculator WatchKit Extension/ContentView.swift
@@ -7,10 +7,63 @@
 
 import SwiftUI
 
+func condition(bmi: Double) -> String {
+    if bmi < 18.5 {
+        return "Underweight 😥"
+    } else if bmi < 24.9 {
+        return "Normal 😃"
+    } else {
+        return "Overweight 😥"
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var heightSlider: Double = 1.50
+    @State private var weightSlider: Double = 70
+    @State private var isEditing = false
+        
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        ScrollView {
+            
+            //Calculate
+            let height = String(format: "%.2f", heightSlider)
+            let weight = String(format: "%.0f", weightSlider)
+            let bmi = Double(weight)! / (Double(height)! * Double(height)!)
+            let bmiToDisplay = String(format: "%.1f", bmi)
+            let textResult = condition(bmi: Double(bmiToDisplay)!)
+            
+            Text("Calculate your BMI").font(.system(size: 22))
+            Divider().colorInvert()
+            
+            //Height
+            VStack {
+                Text("Height: \(height) m")
+                Slider(
+                    value: $heightSlider,
+                    in: 0.00...2.50,
+                    step: 0.01
+                ).accentColor(.green)
+            }
+            Divider().colorInvert()
+            
+            //Weight
+            VStack{
+                Text("Weight: \(weight) kg")
+                Slider(
+                    value: $weightSlider,
+                    in: 0...150,
+                    step: 1
+                ).accentColor(.green)
+            }
+            Divider().colorInvert()
+            
+            VStack {
+                Text("Your BMI value: ")
+                Text("\(bmiToDisplay)").font(.system(size: 35))
+                Text("\(textResult)")
+            }
+        }
     }
 }
 
